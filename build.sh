8e9312c99de934ec5cf10fb812e13e790cb2846f
Fabio Madge <fabio@madge.me>
2012-02-07 16:36:55 +0100

    lightweighted the build script

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 #script takes the necessary classes comiles them and makes a proper .jar
 
-javac EA.java
-javac intReihe.java
-javac steuerung.java
-javac prim.java
-jar cfm primzahlen.jar Manifest EA.class intReihe.class steuerung.class prim.class
-echo 'fertig!'
-echo 'Jetzt: java -jar primzahlen.jar'
+javac *.java
+jar cfm primzahlen.jar Manifest *.classes
+echo 'Fertig!'
+echo 'Zum Ausführen: java -jar primzahlen.jar'
