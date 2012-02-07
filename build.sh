093ad6af11538126a6339e799c234189b8f60027
Fabio Madge <fabio@madge.me>
2012-02-07 19:19:17 +0100

    better user interaction

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #script takes the necessary classes comiles them and makes a proper .jar
 
+echo 'compiling .java files...'
 javac *.java
+echo 'building .jar file...'
 jar cfm primzahlen.jar Manifest *.class
 echo 'Fertig!'
 echo 'Zum Ausführen: java -jar primzahlen.jar'
