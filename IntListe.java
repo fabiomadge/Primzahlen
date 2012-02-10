f6e6ee1a0978ea4aee4aa793a957f74112988ff6
Fabio Madge <fabio@madge.me>
2012-02-10 16:57:06 +0100

    dummy

--- IntListe.java
+++ IntListe.java
@@ -1,6 +1,33 @@
 public class IntListe
 {
-    public IntListe()
-    {
-    }
+
+//String-Darstellung einer IntListe.
+	public String toString(){
+		return "-1";
+	}
+
+//sagt, ob die Liste leer ist.
+	public Boolean istLeer(IntListe is){
+		return false;
+	}
+
+//gibt das vorderste Listenelement zurück.
+	public int kopf(IntListe is){
+		return -1;
+	}
+
+//gibt den Rest der Liste ohne das vorderste Element zurück.
+	public IntListe rest(IntListe is){
+		return is;
+	}
+
+//gibt den Anfang der Liste ohne das hinterste Element zurück.
+	public IntListe anfang(IntListe is){
+		return is;
+	}
+
+//gibt das hinterste Listenelement zurück.
+	public int ende(IntListe is){
+		return -1;
+	}
 }
