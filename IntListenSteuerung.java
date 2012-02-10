961d2bd66857618410eae2602b729a0e3695873b
Fabio Madge <fabio@madge.me>
2012-02-10 16:34:32 +0100

    dummy

--- IntListenSteuerung.java
+++ IntListenSteuerung.java
@@ -1,6 +1,23 @@
 public class IntListenSteuerung
 {
-    public IntListenSteuerung()
-    {
-    }
+
+//erzeugt eine neue, leere IntListe.	
+	public IntListe neu(){
+		return new IntListe();
+	}
+
+//fügt eine Ganzzahl vorne an eine Liste an.
+	public IntListe anfuegen(int i, IntListe is){
+		return is;
+	}
+
+//löscht die übergebene Ganzzahl aus der übergebenen Liste.
+	public IntListe loeschen(int i, IntListe is){
+		return is;
+	}
+
+//erzeugt eine Liste, die alle Zahlen ab 2 bis zur übergebenen Ganzzahl enthält.
+	public IntListe liste(int i){
+		return new IntListe();
+	}
 }
